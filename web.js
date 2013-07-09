18fba6493f341836b184d73ae3b901a7ad62c7ed
Bill Weiland <willbeiland@gmail.com>
2013-07-08 22:25:08 -0400

    read message from index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var msgBuffer = fs.readFileSync('index.html');
+var msgString = Buffer.toString(msgBuffer);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ response.send(msgString);
 });
 
 var port = process.env.PORT || 5000;
